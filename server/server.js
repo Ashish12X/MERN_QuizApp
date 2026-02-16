749797ea2db2492e64e1e516125c3f137a93e0ee
Ashish <ashishkumarkeshri8822.com>
2026-02-17 01:45:10 +0530

    Fix CORS for Netlify frontend

--- server/server.js
+++ server/server.js
@@ -10,7 +10,10 @@ connectDB();
 let app=express();
 app.use(express.json());
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin: [
+    "http://localhost:5173",
+    "https://quizzer-apps.netlify.app"
+  ],
     credentials:true
 }));
 app.use('/api/user',userRoute);
